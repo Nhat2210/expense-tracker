c6dd5178f9c969720e7dd6768553a480adf3b3a5
Nhat2210 <101515915+Nhat2210@users.noreply.github.com>
2025-02-14 18:10:28 +0700

    install supabase and create db

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@nuxtjs/supabase'],
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  supabase: {
+    redirect: false,
+  },
+
 })
